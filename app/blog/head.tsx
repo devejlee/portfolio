e2165dcdc505a5d6bd2c707634e47c0e42b2b66f
Elijah Lee <develijahlee@gmail.com>
2022-12-06 12:33:22 +0900

    feat: create DefaultTags component

---
+++ app/blog/head.tsx
@@ -0,0 +1,11 @@
+import DefaultTags from "@components/DefaultTags";
+
+export default function Head() {
+  return (
+    <>
+      <title>Elijah Lee&apos;s Portfolio | Blog</title>
+      <meta name="description" content="Blog of Frontend Developer Elijah Lee" />
+      <DefaultTags />
+    </>
+  );
+}
--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,11 @@
+import DefaultTags from "@components/DefaultTags";
+
 export default function Head() {
   return (
     <>
-      <title>Elijah Lee&apos;s Portfolio</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <title>Elijah Lee&apos;s Portfolio | Home</title>
+      <meta name="description" content="Frontend Developer porfolio of Elijah Lee" />
+      <DefaultTags />
     </>
   );
 }
