db9b7867451b2aedb2aac9bd190ab5c5137c7f68
Elijah Lee <develijahlee@gmail.com>
2022-12-07 14:35:42 +0900

    feat: add next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
+import styles from "@app/layout.module.scss"
 import { GlobalHeader } from "@components/GlobalHeader/GlobalHeader";
-import '@styles/globals.scss'
-import styles from '@app/layout.module.scss'
+import { Inter } from "@next/font/google";
+import "@styles/globals.scss"
 import { ReactNode } from "react";
+
+const inter = Inter({ subsets: ['latin'] })
 
 interface RootLayoutProps {
   children: ReactNode
 }
 
 const RootLayout = ({ children }: RootLayoutProps) => (
-  <html lang="en">
+  <html lang="en" className={inter.className}>
     <head />
     <body>
       <div className={styles.wrapper}>
