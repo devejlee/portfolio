283c882b20a7ac827a53fd5c4fd322bcb8664995
Elijah Lee <develijahlee@gmail.com>
2022-12-08 16:03:32 +0900

    feat: install tailwind

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -4,6 +4,7 @@ const Intro = () => (
   <section className={styles.wrapper}>
     <h2>Hi! I&apos;m Elijah,</h2>
     <p>I am a frontend developer. Let&apos;s build something great together</p>
+    <p>testing 123</p>
   </section>
 )
 
