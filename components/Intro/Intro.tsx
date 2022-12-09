d93e515c0dc2cb12162de524919555db0390c897
Elijah Lee <develijahlee@gmail.com>
2022-12-09 16:09:16 +0900

    feat: add text mixins

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -3,8 +3,12 @@ import styles from "@components/Intro/Intro.module.scss"
 const Intro = () => (
   <section className={styles.wrapper}>
     <h2>Hi! I&apos;m Elijah,</h2>
-    <p>I am a frontend developer. Let&apos;s build something great together</p>
-    <p>testing 123</p>
+    <p>I am a frontend developer passionate about pushing myself to learn best practices to build awesome web applications</p>
+    <ul className={styles.socialLinks}>
+      <li><a href="#">Resume</a></li>
+      <li><a href="#">LinkedIn</a></li>
+      <li><a href="#">Github</a></li>
+    </ul>
   </section>
 )
 
