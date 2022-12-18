01986c3a64789b97a2cd06ba100065889a758de8
Elijah Lee <develijahlee@gmail.com>
2022-12-18 21:42:34 +0900

    feat: add useFadeIn to projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,18 @@
-const ProjectsPage = () => (
-  <div>This is a projects page</div>
-)
+"use client"
+import { useFadeIn } from "@hooks/useFadeIn"
+
+const ProjectsPage = () => {
+  const fadeInTitle = useFadeIn<HTMLHeadingElement>(100);
+  const fadeInIntro = useFadeIn<HTMLParagraphElement>(200);
+
+  return (
+    <>
+      <section>
+        <h2 {...fadeInTitle} className="will-fade"><span className="text-gradient">Projects</span></h2>
+        <p {...fadeInIntro} className="will-fade">Things I made for fun</p>
+      </section>
+    </>
+  )
+}
 
 export default ProjectsPage
