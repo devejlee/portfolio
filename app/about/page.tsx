0f30c1c8b1b7a286cb48795ee13c97b5b8c5f36f
Elijah Lee <develijahlee@gmail.com>
2022-12-18 17:45:01 +0900

    feat: add skillsWrap class

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -50,7 +50,7 @@ const AboutPage = () => {
         <p {...fadeInIntro} className="will-fade">Hey, I&apos;m Elijah! I started making websites with WordPress as a freshman college student in 2015 as a hobby. That passion persisted for years as I dived into the vast and ever-expanding world of frontend development. I love how coding gives me the freedom to convert my ideas into reality, so long as I put in the time and effort.</p>
         <p {...fadeInExperience} className="will-fade">I have 2+ years of professional experience working as a frontend developer. I have done everything from creating that pixel-perfect layout with CSS, managing and debugging complex client and server states with Redux and React Query, and even working on web3 and NFT projects. I am always open to learning something new.</p>
       </section>
-      <section {...fadeInSkills} className="will-fade">
+      <section {...fadeInSkills} className={`${styles.skillsWrap} will-fade`}>
         <h3>Skills</h3>
         <div className={styles.iconsWrapper}>
           {skills.map((skill) => (
