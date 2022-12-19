a5c05d9347002cad8b3802d8eb1890114054edf1
Elijah Lee <develijahlee@gmail.com>
2022-12-19 15:22:25 +0900

    feat: add fadeInProjects fade in animation

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,13 +7,14 @@ import { useFadeIn } from "@hooks/useFadeIn"
 const ProjectsPage = () => {
   const fadeInTitle = useFadeIn<HTMLHeadingElement>(100);
   const fadeInIntro = useFadeIn<HTMLParagraphElement>(200);
+  const fadeInProjects = useFadeIn<HTMLDivElement>(300);
 
   return (
     <>
       <section className={styles.wrapper}>
         <h2 {...fadeInTitle} className="will-fade"><span className="text-gradient">Projects</span></h2>
         <p {...fadeInIntro} className="will-fade">Things I made for fun</p>
-        <div className={styles.cardsWrapper}>
+        <div {...fadeInProjects} className={`${styles.cardsWrapper} will-fade`}>
           {projects.map((project) => {
             return (
               <Card key={project.title} year={project.year} title={project.title} body={project.body} demoUrl={project.demoUrl} githubUrl={project.githubUrl} />
