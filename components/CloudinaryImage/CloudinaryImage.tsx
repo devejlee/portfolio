cb4cbfdab4ccb3bd4de2e264a5e5ab76508b658a
Elijah Lee <develijahlee@gmail.com>
2022-12-29 16:19:29 +0900

    feat: add urlBlurred to CloudinaryImage

--- components/CloudinaryImage/CloudinaryImage.tsx
+++ components/CloudinaryImage/CloudinaryImage.tsx
@@ -7,27 +7,49 @@ type CloudinaryImageProps = Omit<ImageProps, 'src'> & {
 }
 
 const CloudinaryImage = ({ id, alt, width, height, fill, loader, quality, priority, loading, placeholder, blurDataURL, unoptimized, onLoadingComplete }: CloudinaryImageProps) => {
+  const urlBlurred = buildUrl(id, {
+    cloud: {
+      cloudName: 'drcrnibbz',
+    },
+    transformations: {
+      effect: 'blur:1000',
+      quality: 1
+    }
+  });
+
   const url = buildUrl(id, {
     cloud: {
       cloudName: 'drcrnibbz',
     },
     transformations: {}
   });
+
   return (
-    <Image
-      src={url}
-      alt={alt}
-      width={width}
-      height={height}
-      fill={fill}
-      loader={loader}
-      quality={quality}
-      priority={priority}
-      loading={loading}
-      placeholder={placeholder}
-      blurDataURL={blurDataURL}
-      unoptimized={unoptimized}
-      onLoadingComplete={onLoadingComplete} />
+    <div style={{
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      bottom: '0',
+      right: '0',
+      backgroundImage: `url(${urlBlurred})`,
+      backgroundPosition: 'center center',
+      backgroundSize: '100%'
+    }}>
+      <Image
+        src={url}
+        alt={alt}
+        width={width}
+        height={height}
+        fill={fill}
+        loader={loader}
+        quality={quality}
+        priority={priority}
+        loading={loading}
+        placeholder={placeholder}
+        blurDataURL={blurDataURL}
+        unoptimized={unoptimized}
+        onLoadingComplete={onLoadingComplete} />
+    </div>
   );
 };
 
