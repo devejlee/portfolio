d79fd7af787bd0ca7b0202cc518f12d220716a08
Elijah Lee <develijahlee@gmail.com>
2023-01-02 16:22:01 +0900

    feat: add useSWR to ViewCounter

--- components/ViewCounter/ViewCounter.tsx
+++ components/ViewCounter/ViewCounter.tsx
@@ -1,45 +1,60 @@
 'use client';
 import styles from '@components/ViewCounter/ViewCounter.module.scss';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { AiOutlineEye } from 'react-icons/ai';
+import useSWR from 'swr';
 
 interface ViewCounterProps {
   blogPage: boolean
   slug: string
 }
 
+const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  return res.json();
+};
+
 const ViewCounter = ({ blogPage, slug }: ViewCounterProps) => {
-  const [viewCount, setViewCount] = useState(0);
+  const views = useSWR(
+    `/api/views/?slug=${slug}`,
+    fetcher
+  );
 
   useEffect(() => {
-    if (blogPage) {
-      const getView = async () => {
-        const response = await fetch(`/api/views/?slug=${slug}`, {
-          method: 'GET',
-        });
-        const data = await response.json();
-        setViewCount(data.total);
-      };
-
-      getView();
-      return;
-    }
+    if (blogPage) return;
 
     const registerView = async () => {
       const response = await fetch(`/api/views/?slug=${slug}`, {
         method: 'POST',
       });
       const data = await response.json();
-      setViewCount(data.total);
     };
 
     registerView();
   }, [blogPage, slug]);
 
+  if (views.error) {
+    return (
+      <div className={styles.wrapper}>
+        <AiOutlineEye />
+        <span>error</span>
+      </div>
+    );
+  }
+
+  if (views.isLoading) {
+    return (
+      <div className={styles.wrapper}>
+        <AiOutlineEye />
+        <span>— views</span>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.wrapper}>
       <AiOutlineEye />
-      <span>{viewCount > 0 ? viewCount : '—'} views</span>
+      <span>{views.data.total > 0 ? views.data.total : 0} views</span>
     </div>
   );
 };
