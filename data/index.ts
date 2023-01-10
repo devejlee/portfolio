f4d7022cbfc384f2ae32bb2537d34bf0ef0f3aec
Elijah Lee <develijahlee@gmail.com>
2023-01-10 18:55:28 +0900

    feat: add pizza legends to projects

--- data/index.ts
+++ data/index.ts
@@ -6,4 +6,11 @@ export const projects = [
     demoUrl: 'https://pokedex-one-murex.vercel.app/',
     githubUrl: 'https://github.com/devejlee/pokedex'
   },
+  {
+    year: '2022',
+    title: 'Pizza Legends',
+    body: 'A Pizza Battle game made with HTML, CSS, JS, and OOP',
+    demoUrl: 'https://devejlee.github.io/pizza-legends/',
+    githubUrl: 'https://github.com/devejlee/pizza-legends'
+  },
 ];
