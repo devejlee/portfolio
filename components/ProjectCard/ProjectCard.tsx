2dd7352f31b2d3046745668a8754745b8331cc91
Elijah Lee <develijahlee@gmail.com>
2023-01-12 21:20:09 +0900

    feat: add light and dark theme to ProjectCard

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -1,4 +1,7 @@
+'use client';
 import styles from '@components/ProjectCard/ProjectCard.module.scss';
+import { ThemeContext } from '@store/theme';
+import { useContext } from 'react';
 import { HiExternalLink } from 'react-icons/hi';
 
 interface ProjectCardProps {
@@ -10,14 +13,15 @@ interface ProjectCardProps {
 }
 
 const ProjectCard = ({ year, title, body, demoUrl, githubUrl }: ProjectCardProps) => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${theme === 'light' ? styles.cardLight : styles.cardDark}`}>
       <div>
         <time className="text-gradient">{year}</time>
         <h3>{title}</h3>
         <p>{body}</p>
       </div>
-      <div className={styles.cardLinks}>
+      <div className={`${styles.cardLinks} ${theme === 'light' ? styles.cardLinksLight : styles.cardLinksDark}`}>
         <a href={demoUrl} target="_blank" rel="noopener noreferrer">
           <span>Demo</span>
           <HiExternalLink />
