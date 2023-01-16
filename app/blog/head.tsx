8f08b58667082550924ceb0940f8a012ab76823d
Elijah Lee <develijahlee@gmail.com>
2023-01-16 14:05:23 +0900

    delete: remove meta og tags

--- app/blog/head.tsx
+++ app/blog/head.tsx
@@ -4,18 +4,6 @@ const BlogHead = () => (
   <>
     <title>Elijah Lee&apos;s Portfolio | Blog</title>
     <meta name="description" content="Blog of Frontend Developer Elijah Lee" />
-    <meta property="og:url" content="https://devejlee-portfolio.vercel.app/blog" />
-    <meta property="og:type" content="website" />
-    <meta
-      property="og:title"
-      content="Elijah Lee&apos;s Portfolio | Blog"
-    />
-    <meta
-      property="og:description"
-      content="Blog of Frontend Developer Elijah Lee"
-    />
-    <meta property="og:image" content="/social-media.png" />
-    <meta name="twitter:card" content="Blog of Frontend Developer Elijah Lee" />
     <DefaultHeadTags />
   </>
 );
--- app/head.tsx
+++ app/head.tsx
@@ -4,18 +4,6 @@ const Head = () => (
   <>
     <title>Elijah Lee&apos;s Portfolio | Home</title>
     <meta name="description" content="Frontend Developer porfolio of Elijah Lee" />
-    <meta property="og:url" content="https://devejlee-portfolio.vercel.app/" />
-    <meta property="og:type" content="website" />
-    <meta
-      property="og:title"
-      content="Elijah Lee&apos;s Portfolio | Home"
-    />
-    <meta
-      property="og:description"
-      content="Home of Frontend Developer Elijah Lee"
-    />
-    <meta property="og:image" content="/social-media.png" />
-    <meta name="twitter:card" content="Home of Frontend Developer Elijah Lee" />
     <DefaultHeadTags />
   </>
 );
