ef6b4b815077dd9a825c74c0d5101bfaa7dec8e6
Elijah Lee <develijahlee@gmail.com>
2023-01-16 12:51:54 +0900

    feat: add social media og tags

--- app/about/head.tsx
+++ app/about/head.tsx
@@ -4,6 +4,18 @@ const AboutHead = () => (
   <>
     <title>Elijah Lee&apos;s Portfolio | About</title>
     <meta name="description" content="About Frontend Developer Elijah Lee" />
+    <meta property="og:url" content="https://devejlee-portfolio.vercel.app/about" />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:title"
+      content="Elijah Lee&apos;s Portfolio | About"
+    />
+    <meta
+      property="og:description"
+      content="About Frontend Developer Elijah Lee"
+    />
+    <meta property="og:image" content="/social-media.png" />
+    <meta name="twitter:card" content="About Frontend Developer Elijah Lee" />
     <DefaultHeadTags />
   </>
 );
--- app/projects/head.tsx
+++ app/projects/head.tsx
@@ -4,6 +4,18 @@ const ProjectsHead = () => (
   <>
     <title>Elijah Lee&apos;s Portfolio | Projects</title>
     <meta name="description" content="Projects of Frontend Developer Elijah Lee" />
+    <meta property="og:url" content="https://devejlee-portfolio.vercel.app/projects" />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:title"
+      content="Elijah Lee&apos;s Portfolio | Projects"
+    />
+    <meta
+      property="og:description"
+      content="Projects of Frontend Developer Elijah Lee"
+    />
+    <meta property="og:image" content="/social-media.png" />
+    <meta name="twitter:card" content="Projects of Frontend Developer Elijah Lee" />
     <DefaultHeadTags />
   </>
 );
