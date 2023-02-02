c35f42333f3a643223e1e08ef54f6fe7cbece1cd
Elijah Lee <develijahlee@gmail.com>
2023-02-02 16:23:06 +0900

    feat: add Live Chat to projects

--- data/index.ts
+++ data/index.ts
@@ -1,16 +1,16 @@
 export const projects = [
   {
+    year: '2023',
+    title: 'Live Chat',
+    body: 'Chat app made with Nextjs 13, NextAuth, and Firestore',
+    demoUrl: 'https://chat-devejlee.vercel.app/',
+    githubUrl: 'https://github.com/devejlee/chat'
+  },
+  {
     year: '2022',
     title: 'Pokedex',
     body: 'A Pokedex made with React Query and ChakraUI',
     demoUrl: 'https://pokedex-one-murex.vercel.app/',
     githubUrl: 'https://github.com/devejlee/pokedex'
-  },
-  {
-    year: '2022',
-    title: 'Pizza Legends',
-    body: 'A Pizza Battle game made with HTML, CSS, JS, and OOP',
-    demoUrl: 'https://devejlee.github.io/pizza-legends/',
-    githubUrl: 'https://github.com/devejlee/pizza-legends'
   },
 ];
